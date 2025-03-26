b9dd0f4763c530a0fe9e5f0cebdbed3f7eb7e963
Victor Baldin <victor.baldin.2005@gmail.com>
2025-03-26 23:46:31 +0300

    add draft matrix chain

--- matrix/include/matrix/matrix_chain.hh
+++ matrix/include/matrix/matrix_chain.hh
@@ -1,6 +1,25 @@
 #pragma once
 
+#include <vector>
+
 #include "matrix.hh"
 
+namespace matrix {
+
 template <typename T>
-class MatrixChain final {};
+class MatrixChain final {
+ public:
+  MatrixChain() = default;
+
+  template <typename... Args>
+  auto emplace(Args&&... args) {
+    matrices_.emplace_back(std::forward(args)...);
+  }
+
+  auto multiply() const {}
+
+ private:
+  std::vector<Matrix<T>> matrices_;
+};
+
+}  // namespace matrix
